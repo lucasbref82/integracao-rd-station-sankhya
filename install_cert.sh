8e7484785965eeee86ce0fdb0a2f6fd54e2900b3
wander.silva <wander.silva@digicade.com.br>
2022-08-11 13:56:29 -0300

    correção script.

--- install_cert.sh
+++ install_cert.sh
@@ -5,8 +5,8 @@ do
 	echo $host
 	if [ ! -z ${host+x} ] && [ ! -f $host.cert ]; then
 		echo $host
-		openssl s_client -showcerts -servername $host -connect $host:443 </dev/null | openssl x509 -outform pem > $host.cert && \
-		keytool -import -noprompt -trustcacerts -alias $host -file $host.cert -keystore $JAVA_HOME/lib/security/cacerts -storepass changeit
+		openssl s_client -showcerts -servername $host -connect $host:443 </dev/null | openssl x509 -outform pem > /tmp/$host.cert && \
+		keytool -import -noprompt -trustcacerts -alias $host -file /tmp/$host.cert -keystore $JAVA_HOME/lib/security/cacerts -storepass changeit
 	  
 	fi
 done
